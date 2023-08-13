ab61903be026421c5b59f0d731595c36645c0f61
pickled-dict <mathew_forgione@proton.me>
2023-08-13 15:14:00 -0300

    styling for crud operations on todolist

--- src/lib/components/dashboard/UserTodoLists.svelte
+++ src/lib/components/dashboard/UserTodoLists.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import { TODOLIST, sendGetRequest } from "$lib/fetchRequests";
 	import { onMount } from "svelte";
-  import { signedIn } from "$lib/store";
+  import Icon from "@iconify/svelte";
   import Cookies from "js-cookie";
+  import { TODOLIST, sendGetRequest } from "$lib/fetchRequests";
+  import { signedIn } from "$lib/store";
   
   interface Todo {
     id: number,
@@ -43,14 +44,27 @@
 </script>
 
 <div class="bg-gray-200 w-[250px] text-black">
-  <div class="flex justify-start p-2 pl-3 bg-gray-100">
-    <h3 class="text-2xl font-bold">Your TodoLists</h3>
+  <div class="flex h-14 bg-gray-100 border border-b-black justify-between">
+    <div class="flex items-center ml-2">
+      <h3 class="text-2xl font-bold">Your TodoLists</h3>
+    </div>
+    <div class="flex items-end">
+      <div class="bg-red-500 h-8 w-8 m-1 rounded-full flex justify-center items-center hover:cursor-pointer">
+        <Icon class="text-white h-7 w-7" icon="mingcute:add-line" />
+      </div>
+    </div>
   </div>
   <div class="flex">
-    <div class="ml-2 flex flex-col items-start">
+    <div class="flex flex-col items-start w-full">
       {#if isSignedIn}
       {#each todoLists as tl }
-          <p>{stringShorten(tl.title, 30)}</p>
+          <div class="border border-b-black w-full flex justify-between">
+            <p class="ml-1 my-[1px]">{stringShorten(tl.title, 25)}</p>
+            <div class="h-full w-[40px] flex items-center">
+              <Icon icon="mingcute:edit-2-line"  class="w-[25px] h-[25px] hover:cursor-pointer"/>
+              <Icon icon="mingcute:delete-2-line" class="w-[25px] h-[25px] hover:cursor-pointer" />
+            </div>
+          </div>
       {/each}
       {:else}
         <p>Sign in to view saved todolists!</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,7 @@
 <script lang="ts">
-  import UserTodoLists from "$lib/components/dashboard/UserTodoLists.svelte";
 	import Dashboard from "$lib/components/dashboard/dashboard.svelte";
-import {signedIn} from "$lib/store"
 
-  let isUserSignedIn: boolean;
-
-  signedIn.subscribe((val) => {
-    isUserSignedIn = val;
-  })
 </script>
 
-<div class="text-center text-white">
-  {#if isUserSignedIn}
-    <p>Welcome, user!</p>
-    <Dashboard />
-    {:else}
-      <p>Not logged in</p>
-  {/if}
-</div>
+<Dashboard />
 
